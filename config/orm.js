132c0878d8b3e8471f877cb46c47f31c07d9799f
Evan Morisato <evanmorisato@yahoo.com>
2020-03-14 09:54:55 -0500

    Files Partially Set Up

---
+++ config/orm.js
@@ -0,0 +1,14 @@
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    };
+    insertOne: function(tableInput, colToInput, )
+}
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("hamburger", 2.50);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("cheeseburger", 3.10);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("bacon cheeseburger", 4.25);
