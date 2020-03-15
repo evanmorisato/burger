504be85823057a55c1e739a7fa24af366fd1441c
Evan Morisato <evanmorisato@yahoo.com>
2020-03-14 23:05:35 -0500

    controller file updated

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,18 +16,25 @@ router.post('/burgers', function(req, res) {
         'burger_name'
     ], [
         req.body.burger_name
-    ], function(data) {
-        res.redirect('/');
+    ], function(result) {
+        res.json({ id: result.insertId });
     });
 });
 
 router.put('/burgers/:id', function(req, res) {
     var condition = 'id = ' + req.params.id;
 
+    console.log("condition", condition);
+
     burger.updateOne({
         devoured: true
-    }, condition, function(data) {
-        res.redirect('/');
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+          } else {
+            res.status(200).end();
+          }
     });
 });
 
