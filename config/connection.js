16d361025d79a80fc303ab70862d8e9f5c3b4cc7
evanmorisato <evanmorisato@yahoo.com>
2023-01-24 11:05:53 -0600

    Updated db connectivity. Next step to modify delete functionality

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var connection;
+require('dotenv').config();
 
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -8,7 +9,7 @@ if (process.env.JAWSDB_URL) {
 		host: "localhost",
     port: 3306,
     user: "root",
-    password: "Toshio510!",
+    password: process.env.PERSONAL_PW,
     database: "burgers_db"
 	});
 }
